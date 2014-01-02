66c7893f5f106e085ecc5f8329fc3f558ceae98e
Matthew Weier O'Phinney <matthew@zend.com>
2014-01-02 11:00:57 -0600

    zendframework/zf2@614db035c2be7b31e6ece497f1ef513ee5aded15 (1388506301)

--- Container.php
+++ Container.php
@@ -3,13 +3,13 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
 namespace Zend\Session;
 
-if (version_compare(PHP_VERSION, '5.3.4', 'lt')) {
+if (PHP_VERSION_ID < 50304) {
     class_alias('Zend\Session\AbstractContainer', 'Zend\Session\AbstractBaseContainer');
 } else {
     class_alias('Zend\Session\Container\PhpReferenceCompatibility', 'Zend\Session\AbstractBaseContainer');
--- Storage/SessionArrayStorage.php
+++ Storage/SessionArrayStorage.php
@@ -3,13 +3,13 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
 namespace Zend\Session\Storage;
 
-if (version_compare(PHP_VERSION, '5.3.4', 'lt')) {
+if (PHP_VERSION_ID < 50304) {
     class_alias('Zend\Session\Storage\AbstractSessionArrayStorage', 'Zend\Session\Storage\AbstractBaseSessionArrayStorage');
 } else {
     class_alias('Zend\Session\Storage\SessionArrayStorage\PhpReferenceCompatibility', 'Zend\Session\Storage\AbstractBaseSessionArrayStorage');
